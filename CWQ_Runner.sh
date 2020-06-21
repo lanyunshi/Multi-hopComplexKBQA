a7ab1bc90dbbb7e088c684f5a4ad04aeda5238e7
Lan Yunshi <lanyunshi@Lans-MacBook-Pro.local>
2020-06-21 17:10:43 +0800

    second commit

--- CWQ_Runner.sh
+++ CWQ_Runner.sh
@@ -1,6 +1,6 @@
 python code/KBQA_Runner.py  \
-        --train_folder  data/test_CWQ \
-        --dev_folder data/test_CWQ \
+        --train_folder  data/train_CWQ \
+        --dev_folder data/dev_CWQ \
         --test_folder data/test_CWQ \
         --vocab_file data/CWQ/vocab.txt \
         --KB_file data/CWQ/kb_cache.json \
@@ -8,12 +8,12 @@ python code/KBQA_Runner.py  \
         --QUERY_file data/CWQ/query_cache.json \
         --output_dir trained_model/CWQ \
         --config config/bert_config.json \
-        --gpu_id 3\
-        --load_model trained_model/CWQ/Bert_Best \
-        --save_model Bert_Best2 \
+        --gpu_id 1\
+        --load_model trained_model/CWQ/Best \
+        --save_model Best \
         --max_hop_num 3 \
         --num_train_epochs 30 \
-        --do_train 1\
+        --do_train 0\
         --do_eval 1\
         --do_policy_gradient 1\
         --learning_rate 5e-5 \
